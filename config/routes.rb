1a9892ce4dfe0dce74741cc7516b22b37edcf300
Lugard Agu <agulugard@gmail.com>
2023-08-01 15:20:32 +0100

    Created users controller and views and update users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "users#index"
+
+  resources :users
+  resources :foods, only: %i[index new create destroy]
+  resources :recipes, only: %i[index show new create destroy] do
+    resources :recipe_foods
+  end
+
+  resources :public_recipes, only: [:index]
+  resources :general_shopping_list, only: [:index]
 end
