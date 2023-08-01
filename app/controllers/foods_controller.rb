822951ed16df63ea8b2e040183547328e2deb5e5
eudondian <eudondian@gmail.com>
2023-08-01 23:49:20 +0100

    Add foods controllers

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,43 @@
 class FoodsController < ApplicationController
-end
+    before_action :set_food, only: [:show, :edit,  :destroy]
+  
+    def index
+      @foods = Food.all.where(user_id: current_user.id)
+    end
+  
+    def show
+    end
+  
+    def new
+      @food = Food.new
+    end
+  
+    def create
+      @food = current_user.foods.new(food_params)
+  
+      if @food.save
+        redirect_to @foods_path, notice: 'Food was successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    def edit
+    end
+  
+    
+    def destroy
+      @food.destroy
+      redirect_to foods_path, notice: 'Food was successfully destroyed.'
+    end
+  
+    private
+  
+    def set_food
+      @food = Food.find(params[:id])
+    end
+  
+    def food_params
+      params.require(:food).permit(:name, :measurement_unit, :price)
+    end
+  end
