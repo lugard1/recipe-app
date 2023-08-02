52ae465462f8162760da0061e0fcca20c75fed8b
eudondian <eudondian@gmail.com>
2023-08-02 21:42:35 +0100

    Fix rubocop error

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,5 @@
 class FoodsController < ApplicationController
-  before_action :set_food, only: [:show, :edit, :update, :destroy]
+  before_action :set_food, only: %i[show edit update destroy]
 
   def index
     @foods = Food.all
