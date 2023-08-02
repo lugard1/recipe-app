b4cd0521ee48af5108e165cb5926f9926c5b7849
eudondian <eudondian@gmail.com>
2023-08-02 02:05:45 +0100

    Fix rubocop errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,37 +1,35 @@
 class FoodsController < ApplicationController
-       before_action :set_food, only: [:show, :edit, :update, :destroy]
-  
-    def index
-      @foods = Food.all.where(user: current_user)
-    end
-  
-    def show
-    end
-  
-    def new
-      @food = Food.new
-    end
-  
-    def create
-      @food = current_user.food.new(food_params)
-  
-      if @food.save
-        redirect_to @foods_path, notice: 'Food was successfully created.'
-      else
-        render :new
-      end
-    end
-  
-    def edit
-    end
-  
-    private
-  
-    def set_food
-      @food = Food.find(params[:id])
-    end
-  
-    def food_params
-      params.require(:food).permit(:name, :measurement_unit, :price)
+  before_action :set_food, only: %i[show edit update destroy]
+
+  def index
+    @foods = Food.all.where(user: current_user)
+  end
+
+  def show; end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = current_user.food.new(food_params)
+
+    if @food.save
+      redirect_to @foods_path, notice: 'Food was successfully created.'
+    else
+      render :new
     end
   end
+
+  def edit; end
+
+  private
+
+  def set_food
+    @food = Food.find(params[:id])
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
+end
