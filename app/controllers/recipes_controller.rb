6d189b16c14fa84678c6e6456adbe01cf96a6eeb
Lugard Agu <agulugard@gmail.com>
2023-08-04 12:41:18 +0100

    Update recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,57 +1,54 @@
 class RecipesController < ApplicationController
-  class RecipesController < ApplicationController
-    before_action :set_recipe, only: %i[show edit update destroy]
+  before_action :set_recipe, only: %i[show edit update destroy]
   
-    def index
-      @recipes = Recipe.all
-    end
-  
-    def show
-    end
-  
-    def new
-      @recipe = Recipe.new
-    end
-  
-    def edit
-    end
-  
-    def create
-      @recipe = Recipe.new(recipe_params)
-  
-      if @recipe.save
-        redirect_to @recipe, notice: 'Recipe was successfully created.'
-      else
-        render :new, status: :unprocessable_entity
-      end
-    end
-  
-    def update
-      if @recipe.update(recipe_params)
-        redirect_to @recipe, notice: 'Recipe was successfully updated.'
-      else
-        render :edit, status: :unprocessable_entity
-      end
-    end
-  
-    def destroy
-      if @recipe.user_id == current_user.id
-        @recipe.destroy
-        redirect_to recipes_url, notice: 'Recipe was successfully destroyed.'
-      else
-        redirect_to recipes_url, alert: 'You do not have permission to delete this recipe.'
-      end
-    end
-  
-    private
-  
-    def set_recipe
-      @recipe = Recipe.find(params[:id])
-    end
-  
-    def recipe_params
-      params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id)
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def edit
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to @recipe, notice: 'Recipe was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
-  
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe, notice: 'Recipe was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @recipe.user_id == current_user.id
+      @recipe.destroy
+      redirect_to recipes_url, notice: 'Recipe was successfully destroyed.'
+    else
+      redirect_to recipes_url, alert: 'You do not have permission to delete this recipe.'
+    end
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id)
+  end
 end
